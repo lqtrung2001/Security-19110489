7bb91875ee488e86e92659a5e127d2263726bf14
Luong Quoc Trung <lqtrung20122001@gmail.com>
2022-12-03 22:45:56 +0700

    commit

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const https = require('https')
+const fs = require('fs')
 const path = require('path')
+const helmet = require('helmet')
 const express = require('express')
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(helmet())
 
 app.get('/secret', (req, res) =>{
     return res.send('Your persionnal secret value is 100')
@@ -13,6 +18,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
- app.listen(PORT, () =>{
+ https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+ }, app).listen(PORT, () =>{
     console.log(`Listening on port ${PORT}...`);
  })
